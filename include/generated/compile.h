8062babee1eca4c59f330c2b56c41818924fc418
android <rarfbd@gmail.com>
2020-07-31 13:49:26 -0700

    First Commint

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#5 SMP PREEMPT Fri Jul 31 13:01:18 PDT 2020"
+#define LINUX_COMPILE_BY "android"
+#define LINUX_COMPILE_HOST "android-VirtualBox"
+#define LINUX_COMPILER "Android (4639204 based on r316199) clang version 6.0.1 (https://android.googlesource.com/toolchain/clang 279c0d3a962121a6d1d535e7b0b5d9d36d3c829d) (https://android.googlesource.com/toolchain/llvm aadd87ffb6a2eafcb577913073d46b20195a9cdc) (based on LLVM 6.0.1svn)"
