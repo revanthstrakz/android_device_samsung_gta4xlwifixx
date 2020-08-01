b80763a6f457760e210a6b3617420fa0b304cb7c
Revanth Strakz <dr.revanthstrakz@gmail.com>
2020-08-01 21:06:12 +0530

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=/home/android/Desktop/S6TabKernel/toolchain/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=$(pwd)/toolchain/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 
-export clang_triple=/home/android/Desktop/S6TabKernel/kernel/clang/bin/clang
+export clang_triple=$(pwd)/home/android/Desktop/S6TabKernel/kernel/clang/bin/clang
 export ANDROID_MAJOR_VERSION=q
 
 make exynos9610-gta4xlwifixx_defconfig
